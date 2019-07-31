8282f3412281d783afbfd6887dc36811cc731ba2
Home <oa.oni7@gmail.com>
2019-07-30 21:00:50 -0400

    initial commit

---
+++ run_vae.py
@@ -0,0 +1,39 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+import os
+import time
+import numpy as np
+import glob
+import matplotlib.pyplot as plt
+import PIL
+import imageio
+
+
+
+if __name__ == '__main__':
+
+    #Test on mnist data
+    (train_images, _), (test_images, _) = tf.keras.datasets.mnist.load_data()
+
+    train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+    test_images = test_images.reshape(test_images.shape[0], 28, 28, 1).astype('float32')
+
+    # Normalizing the images to the range of [0., 1.]
+    train_images /= 255.
+    test_images /= 255.
+
+    # Binarization
+    train_images[train_images >= .5] = 1.
+    train_images[train_images < .5] = 0.
+    test_images[test_images >= .5] = 1.
+    test_images[test_images < .5] = 0.
+
+    TRAIN_BUF = 60000
+    BATCH_SIZE = 100
+    TEST_BUF = 10000
+
+    train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(TRAIN_BUF).batch(BATCH_SIZE)
+    test_dataset = tf.data.Dataset.from_tensor_slices(test_images).shuffle(TEST_BUF).batch(BATCH_SIZE)
+
+    print('done')
